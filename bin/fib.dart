26c0e7c9a68502d1e833339d4a6ade762fd0eb83
SAJITH S <144709343+sajiths07@users.noreply.github.com>
2023-09-20 10:36:08 +0530

    Update fib.dart

--- bin/fib.dart
+++ bin/fib.dart
@@ -3,10 +3,10 @@ void main() {
   int n = 10; // number of Fibonacci numbers to generate
 
   print("Fibonacci Series:");
-  generateFibonacciSeries(n);
+  FibonacciSeries(n);
 }
 
-void generateFibonacciSeries(int n) {
+void FibonacciSeries(int n) {
   int num1 = 0, num2 = 1;
 
   for (int i = 0; i < n; i++) {
